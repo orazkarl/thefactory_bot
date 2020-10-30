6c19488ecc0a3e4035a6c3639f4c449c9b294d2a
Orazali Karl <43617375+orazkarl@users.noreply.github.com>
2020-10-30 21:39:38 +0600

    Add files via upload

---
+++ thefactory_bot.py
@@ -0,0 +1,15 @@
+import telebot
+bot = telebot.TeleBot('1088972154:AAH10CkfPAvkK22dJxx-ILrl-qUvtY6LRDo')
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
+    user_markup.row('/start', '/yourid')
+    bot.send_message(message.from_user.id, text='hello', reply_markup=user_markup)
+
+
+@bot.message_handler(commands=['yourid'])
+def handle_token(message):
+    bot.send_message(message.from_user.id, text='Your Telegram User ID: ' + message.from_user.id)
+
+bot.polling(none_stop=True)
